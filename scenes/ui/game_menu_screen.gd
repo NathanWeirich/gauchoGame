f5ad078cb5c362953b835174ab378d36b7400653
Nathan Weirich <nathanweirich@gmail.com>
2025-06-30 19:57:41 -0300

    added level themes and other sfx

--- scenes/ui/game_menu_screen.gd
+++ scenes/ui/game_menu_screen.gd
@@ -1,10 +1,26 @@
 extends CanvasLayer
 
 @onready var save_game_button: Button = $MarginContainer/VBoxContainer/SaveGameButton
+@onready var start_game_button: Button = $MarginContainer/VBoxContainer/StartGameButton
+@onready var exit_game_button: Button = $MarginContainer/VBoxContainer/ExitGameButton
+
+@onready var click: AudioStreamPlayer2D = $Click
+@onready var hover: AudioStreamPlayer2D = $Hover
 
 func _ready() -> void:
 	save_game_button.disabled = !SaveGameManager.allow_save_game
 	save_game_button.focus_mode = SaveGameManager.allow_save_game if Control.FOCUS_ALL else Control.FOCUS_NONE 
+	
+	# Conectar sinais de mouse
+	for button in [save_game_button, start_game_button, exit_game_button]:
+		button.mouse_entered.connect(_on_button_mouse_entered)
+		button.pressed.connect(_on_button_pressed)
+
+func _on_button_mouse_entered():
+	hover.play()
+
+func _on_button_pressed():
+	click.play()
 
 func _on_start_game_button_pressed() -> void:
 	GameManager.start_game()
